d1faa3668d3b8e533ff0bc58a3cd8d46b7b530d0
BanialSam <121105677+BanialSam@users.noreply.github.com>
2022-12-20 19:01:43 +0530

    Add files via upload

---
+++ English.java
@@ -0,0 +1,19 @@
+package org.english;
+
+import org.tamil.Tamil;
+
+public class English extends Tamil {
+
+	public void englishlanguage() {
+		
+		System.out.println("English is the universal language");
+	}
+	
+	public static void main(String[] args) {
+		
+		English E=new English();
+				
+		E.englishlanguage();
+		E.TamilLAnguage();
+	}
+}
